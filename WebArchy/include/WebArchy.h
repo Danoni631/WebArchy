e7384d647dc33efa1c5b3f368a3523976f4eb6c2
DaniloSenseiPlus <danunisonsoi@gmail.com>
2026-09-18 10:24:16 -0300

    Add additional header includes to WebArchy.h

--- WebArchy/include/WebArchy.h
+++ WebArchy/include/WebArchy.h
@@ -1,3 +1,13 @@
 #include "std.h"
 #include <stdio.h>
 #include <string.h>
+
+#include "../src/browser.h"
+#include "../src/defs.h"
+#include "../src/others.h"
+#include "../src/render.h"
+#include "../src/view.h"
+
+#include "../src/qjs_include.h"
+#include "../src/qjs_port.h"
+#include "../src/qjs_runtime.h"
